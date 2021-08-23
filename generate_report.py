8e40eb96dd730ad5b36f8f6284c9a41041018107
sinking8 <ashwinblaze111@gmail.com>
2021-08-23 20:19:31 +0530

    added generate_report class

--- generate_report.py
+++ generate_report.py
@@ -1,18 +1,33 @@
 from fpdf import FPDF
 
-# format ('A3','A4','Letter','Legal')
-pdf = FPDF('P','mm','A4')
+class Report:
 
-# Adding a page to the pdf file
-pdf.add_page()
+    # Creating pdf object
+    pdf = None
 
-# Setting up font
-pdf.set_font('helvetica','',16)
+    def __init__(self):
+        # format ('A3','A4','Letter','Legal')
+        self.pdf = FPDF('P','mm','A4')
 
-# Add Text
-# w = width
-# h = height
-pdf.cell(40,10,'Hello World')
+        # Adding a page to the pdf file
+        self.pdf.add_page()
+
+        # Setting up font
+        self.pdf.set_font('helvetica','',16)
 
 
-pdf.output('report.pdf')
+
+    def insert_text(self,user_details):
+        # Add Text
+        # w = width
+        # h = height
+        self.pdf.cell(40,10,user_details['name'])
+
+
+    def generate_report(self):
+        self.insert_text({'name':"Ashwin"})
+        self.pdf.output('report.pdf')
+
+if __name__ == '__main__':
+    report = Report()
+    report.generate_report()
