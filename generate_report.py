7c65342862a6d829816843dcb4ba2c966272c34e
sinking8 <ashwinblaze111@gmail.com>
2021-08-24 00:15:15 +0530

    completed report module

--- generate_report.py
+++ generate_report.py
@@ -1,13 +1,13 @@
 from fpdf import FPDF
 from datetime import date
 
-
-import os
-
 class Report:
 
     # Creating pdf object
     pdf = None
+
+    # inx declaration
+    inx = 60
 
     def __init__(self):
         # format ('A3','A4','Letter','Legal')
@@ -24,18 +24,16 @@ class Report:
         # Arial bold 15
         self.pdf.set_font('Arial', 'B', 15)
 
-        # Date
-        self.pdf.cell(100,10,str(date.today()))
-
         # Move to the right
-        self.pdf.cell(50)
+        self.pdf.cell(46)
 
         # Title
-        self.pdf.cell(80, 10, 'MEDICAL REPORT', 1, 0, 'C')
+        self.pdf.cell(90, 20, 'MEDICAL REPORT', 1, 0, 'C')
 
         # Logo
-        self.pdf.image('./static/images/logo1.jpg', 160, 8, 33)
+        self.pdf.image('./static/images/logo1.jpg', 170, 4, 33)
 
+        self.pdf.line(0, 40,220,40)
 
         # Line break
         self.pdf.ln(20)
@@ -48,18 +46,58 @@ class Report:
         # Adding Title
         # for key,value
 
-        # inx  = 100
-        # for key,value in user_details.items():
-        #     self.pdf.cell(40,inx,key + " : " + value)
-        #     inx+=20
+        inx  = self.inx
+        for key,value in user_details.items():
+            self.pdf.cell(0,inx,key + " : " + value)
+            self.pdf.ln(2)
+            inx+=20
+
+        self.pdf.ln(1)
+        inx+=10
+        self.inx = inx
 
 
     def generate_report(self):
 
-        print(os.getcwd())
+        # print(os.getcwd())
         self.header()
-        self.insert_text({'name':"Ashwin","age":"20"})
-        self.pdf.output('report.pdf')
+
+        # Setting up Personal Details header
+        self.pdf.cell(0,40,"PERSONAL DETAILS")
+        self.pdf.ln(2)
+
+
+        self.insert_text(
+            {
+                'Name':"Ashwin",
+                "Age":"20",
+                "Height":"170",
+                "Weight":"60",
+                "Gender":"Male",
+                
+                }
+                )
+
+        self.pdf.cell(0,self.inx,"ANALYSIS")
+        self.pdf.line(0,120,220,120)
+        self.pdf.ln(16)
+        
+        self.insert_text(
+            {
+                "Reason for Diagnosis":"Fever",
+                "Disease for Analysis":" Glaucoma",
+                "Analysis":"Tested Positive for Glaucoma"
+
+            }
+            )
+
+        # Logo
+        self.pdf.image('./static/images/test_img.jpeg', 110, 50, 90)
+
+        self.pdf.line(0,200,220,200)
+        self.pdf.ln(16)
+
+        self.pdf.output('./reports/report.pdf')
 
 if __name__ == '__main__':
     report = Report()
